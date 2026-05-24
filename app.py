56661fe17c62a21a0023741ffca230c168ebdff3
Atharva Khachane <atharvakhachane642003@gmail.com>
2026-05-24 15:20:38 +0530

    Removed local MySQL dependency

--- app.py
+++ app.py
@@ -62,8 +62,46 @@ st.caption("Built by Atharva Khachane | Real-Time Crypto Analytics Platform")
 # Auto refresh every 60 seconds
 st_autorefresh(interval=60000, key="crypto_refresh")
 
+st.caption(...)
 
-df = pd.read_sql(query, engine)
+import requests
+
+url = "https://api.coingecko.com/api/v3/coins/markets"
+
+params = {
+    "vs_currency": "usd",
+    "order": "market_cap_desc",
+    "per_page": 10,
+    "page": 1,
+    "sparkline": False,
+    "price_change_percentage": "24h"
+}
+
+response = requests.get(url, params=params)
+
+data = response.json()
+
+df = pd.DataFrame(data)
+
+df = df[[
+    "name",
+    "symbol",
+    "current_price",
+    "market_cap",
+    "total_volume",
+    "price_change_percentage_24h"
+]]
+
+df.columns = [
+    "Name",
+    "Symbol",
+    "Current Price",
+    "Market Cap",
+    "Volume",
+    "24h Change %"
+]
+
+filtered_df = df
 
 # Latest records only
 latest_df = df.sort_values("timestamp").groupby("coin_name").tail(1)
