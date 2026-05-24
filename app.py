53a39f4d45a4354e946266bac2cb29ff3bf464ef
Atharva Khachane <atharvakhachane642003@gmail.com>
2026-05-24 15:15:51 +0530

    Fixed Streamlit deployment

--- app.py
+++ app.py
@@ -2,14 +2,48 @@ from streamlit_autorefresh import st_autorefresh
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from sqlalchemy import create_engine
 
-# Database connection
-username = "root"
-password = "password"
-host = "127.0.0.1"
-port = "3306"
-database = "crypto_dashboard"
+st.title("🚀 Cryptocurrency Market Dashboard")
+
+import requests
+
+url = "https://api.coingecko.com/api/v3/coins/markets"
+
+params = {
+    "vs_currency": "usd",
+    "order": "market_cap_desc",
+    "per_page": 10,
+    "page": 1,
+    "sparkline": False,
+    "price_change_percentage": "24h"
+}
+
+response = requests.get(url, params=params)
+
+data = response.json()
+
+df = pd.DataFrame(data)
+
+df = df[[
+    "name",
+    "symbol",
+    "current_price",
+    "market_cap",
+    "total_volume",
+    "price_change_percentage_24h"
+]]
+
+df.columns = [
+    "Name",
+    "Symbol",
+    "Current Price",
+    "Market Cap",
+    "Volume",
+    "24h Change %"
+]
+
+filtered_df = df
+
 
 DATABASE_URL = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
 
@@ -28,10 +62,6 @@ st.caption("Built by Atharva Khachane | Real-Time Crypto Analytics Platform")
 # Auto refresh every 60 seconds
 st_autorefresh(interval=60000, key="crypto_refresh")
 
-# SQL Query
-query = """
-SELECT * FROM crypto_prices
-"""
 
 df = pd.read_sql(query, engine)
 
@@ -149,4 +179,4 @@ fig4 = px.line(
 
 st.plotly_chart(fig4, use_container_width=True)
 
-from forecasting.bitcoin_forecast import *
+
